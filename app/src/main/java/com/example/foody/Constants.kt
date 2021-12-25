f2de140f553e2f36cb07ea3f68e04b72dbb5868a
UlasGuneyGenc <ulasguneygenc@gmail.com>
2021-12-26 00:46:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.foody
+
+class Constants {
+    companion object{
+        const val API_KEY = "af05e1e8f7084e8092c026b77ce504b4"
+        const val BASE_URL = "https://api.spoonacular.com"
+    }
+}
