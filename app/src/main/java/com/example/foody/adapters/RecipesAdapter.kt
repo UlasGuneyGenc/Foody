9baaf87569ccec23db01e6385ec75eaf1ac30dbf
UlasGuneyGenc <ulasguneygenc@gmail.com>
2022-01-01 16:40:29 +0300

    Recipes adapter implemented

---
+++ app/src/main/java/com/example/foody/adapters/RecipesAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.foody.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foody.databinding.RecipesRowLayoutBinding
+
+class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
+    class MyViewHolder(private val binding: RecipesRowLayoutBinding): RecyclerView.ViewHolder(binding.root){
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
