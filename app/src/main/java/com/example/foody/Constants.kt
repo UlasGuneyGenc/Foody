03b37b7a871ce98bf7d09407a7ce0f7803c07bdb
UGG <ulasguneygenc@gmail.com>
2023-02-05 21:26:48 +0300

    Add Retrofit setup for API calls

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.foody
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "af05e1e8f7084e8092c026b77ce504b4"
+    }
+}
