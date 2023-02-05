b597c763431f2c0c9a8f0d36b30020eb8dd30c0c
UGG <ulasguneygenc@gmail.com>
2023-02-05 20:01:10 +0300

    Add Recipes Fragment Card view and setup shimmer effect

--- app/src/main/java/com/example/foody/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/RecipesFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.foody.databinding.ActivityMainBinding
+import com.example.foody.databinding.FragmentRecipesBinding
 
 class RecipesFragment : Fragment() {
+    private lateinit var binding: FragmentRecipesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        binding = FragmentRecipesBinding.inflate(layoutInflater)
+        val view = binding.root
+        binding.recyclerView.showShimmer()
+        return view
     }
 }
