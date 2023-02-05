85558e7a6cd2b576372405ab636d9b8274830c59
UGG <ulasguneygenc@gmail.com>
2023-02-06 02:13:18 +0300

    Add dependency injection and network module, create repository for necessary classes

--- app/src/main/java/com/example/foody/data/network/FoodRecipesApi.kt
+++ app/src/main/java/com/example/foody/data/network/FoodRecipesApi.kt
@@ -1,6 +1,9 @@
-package com.example.foody
+package com.example.foody.data.network
 
 import com.example.foody.models.FoodRecipe
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.QueryMap
