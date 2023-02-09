f53a5f7065457f92814f1f357e7fc3cb78692f75
UGG <ulasguneygenc@gmail.com>
2023-02-09 19:30:59 +0300

    Setup DAO & Add type converters

--- app/src/main/java/com/example/foody/util/Constants.kt
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -13,5 +13,9 @@ class Constants {
         const val QUERY_DIET = "diet"
         const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
         const val QUERY_FILL_INGREDIENTS = "fillIngredients"
+
+        //ROOM Database
+        const val DATABASE_NAME = "recipesDatabase"
+        const val RECIPES_TABLE = "recipesTable"
     }
 }
